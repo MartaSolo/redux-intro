35a0488c94e02eddeed95bb27dfa0510f72c76b3
MartaSolo <marciamarcia@o2.pl>
2022-04-22 18:53:14 +0200

    Adding the store/index.js file, adding Redux to the App and Counter component

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+// reducer
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+// state store
+const store = createStore(counterReducer);
+
+// connecting the components with Redux
+export default store;
